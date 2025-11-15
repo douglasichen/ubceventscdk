3b383b7cb126cc52e002194a349e76b64290571c
Douglas Chen <douglas@ichen.ca>
2025-11-14 21:07:51 -0800

    feat: cdk init

---
+++ lib/ubceventscdk-stack.ts
@@ -0,0 +1,27 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+
+export class UbceventscdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const eventsBucket = new s3.Bucket(this, "UbcEventsBucket", {
+      bucketName: "ubc-events-bucket",
+    });
+
+    // const instagramIdsBucket = new s3.Bucket(this, "InstagramIdsBucket", {
+    //   bucketName: "instagram-ids",
+    // });
+
+    // const processInstagramIdsLambda = new lambda.Function(
+    //   this,
+    //   "ProcessInstagramIdsLambda",
+    //   {
+    //     runtime: lambda.Runtime.NODEJS_22_X,
+    //     code: lambda.Code.fromAsset("lambda/process-instagram-ids"),
+    //     handler: "index.handler",
+    //   }
+    // );
+  }
+}
---
+++ test/ubceventscdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ubceventscdk from '../lib/ubceventscdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ubceventscdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ubceventscdk.UbceventscdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
