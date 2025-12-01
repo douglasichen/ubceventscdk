4dfb5e9c71c78eabfed940cf667190b8d02c3664
Douglas Chen <douglas@ichen.ca>
2025-11-30 17:27:22 -0800

    chore: stub

--- lambda/process-instagram-id/index.py
+++ lambda/process-instagram-id/index.py
@@ -1,47 +1,47 @@
 
-import json
-def process_instagram_id(instagram_id: str):
-    print(f"Processing Instagram ID '{instagram_id}'")
 
+import json
 
 def handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
 
-    instagram_id = event["instagramId"]
-    already_processed = False
+# import json
+# def process_instagram_id(instagram_id: str):
+#     print(f"Processing Instagram ID '{instagram_id}'")
 
-    if already_processed:
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Content-Type": "application/json",
-            },
-            "body": json.dumps({
-                "alreadyProcessed": True,
-                "processed": False,
-            }),
-        }
 
-    try:
-        process_instagram_id(instagram_id)
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Content-Type": "application/json",
-            },
-            "body": json.dumps({
-                "alreadyProcessed": False,
-                "processed": True,
-            }),
-        }
-    except Exception as e:
-        print(f"Error processing Instagram ID '{instagram_id}':", e)
-        return {
-            "statusCode": 500,
-            "headers": {
-                "Content-Type": "application/json",
-            },
-            "body": json.dumps({
-                "alreadyProcessed": False,
-                "processed": False,
-            }),
-        }
+# def handler(event, context):
+    # instagram_id = event["instagramId"]
+    # already_processed = False
+
+    # if already_processed:
+    #     return {
+    #         "statusCode": 200,
+    #         "body": json.dumps({
+    #             "alreadyProcessed": True,
+    #             "processed": False,
+    #         }),
+    #     }
+
+    # try:
+    #     process_instagram_id(instagram_id)
+    #     return {
+    #         "statusCode": 200,
+    #         "body": json.dumps({
+    #             "alreadyProcessed": False,
+    #             "processed": True,
+    #         }),
+    #     }
+    # except Exception as e:
+    #     print(f"Error processing Instagram ID '{instagram_id}':", e)
+    #     return {
+    #         "statusCode": 500,
+    #         "body": json.dumps({
+    #             "alreadyProcessed": False,
+    #             "processed": False,
+    #         }),
+    #     }
