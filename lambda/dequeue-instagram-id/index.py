597ebc6d4a74cd8ee855d4da01564fc79c6c8518
Douglas Chen <douglas@ichen.ca>
2025-11-30 23:44:23 -0800

    feat: ai pipeline working

--- lambda/dequeue-instagram-id/index.py
+++ lambda/dequeue-instagram-id/index.py
@@ -10,20 +10,16 @@ def instagram_id_exists(instagram_id: str, table):
     return response.get("Item", None) is not None
 
 
-def fetch_and_save_instagram_preview(instagram_id: str):
-    url = f"https://www.instagram.com/p/{instagram_id}/"
+def fetch_and_save_instagram_preview(instagram_id: str, instagram_url: str, table):
     link_preview_api_key = os.environ["LINK_PREVIEW_API_KEY"]
     link_preview_api_url = os.environ["LINK_PREVIEW_API_URL"]
     response = requests.get(
         link_preview_api_url,
         headers={"X-Linkpreview-Api-Key": link_preview_api_key},
-        params={"q": url},
+        params={"q": instagram_url},
     )
     preview_data = response.json()
 
-    dynamodb = boto3.resource("dynamodb")
-    table_name = os.environ["DYNAMO_EVENTS_TABLE_NAME"]
-    table = dynamodb.Table(table_name)
     table.update_item(
         Key={"id": instagram_id},
         UpdateExpression="set preview_data = :p",
@@ -36,75 +32,78 @@ def has_food(preview_data: dict):
     return True
 
 
-def generate_ai_data(context: str, base64_image: str):
+def generate_ai_data(context: str, image_bytes: bytes):
     client = boto3.Session().client("bedrock-runtime", region_name="us-east-1")
-    MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0"
+    MODEL_ID = "us.anthropic.claude-haiku-4-5-20251001-v1:0"
     TEMPERATURE = 0
     MAX_TOKENS = 4096
+
+    tool_name = "generate_event_data"
 
     schema = {
         "description": "Schema for summarizing an instagram event post",
         "type": "object",
         "properties": {
-            "title": {
+            "event_name": {
                 "type": "string",
-                "description": "Title of the event.",
-                "maxLength": 25,
+                "description": "name of the event.",
+                "maxLength": 15,
             },
             "description": {
                 "type": "string",
-                "description": "A description summary that is only to 1-2 sentences. Only whats happening at the event. Leave out time, place and event title. Keep it simple and fun",
+                "description": "A description summary that is only to 1-2 sentences. Only whats happening at the event. Leave out time, place and event name. Keep it simple and fun",
                 "maxLength": 300,
             },
+            "has_food_or_drinks": {
+                "type": "boolean",
+                "description": "Whether the post has food or drinks.",
+            },
+            "food_or_drinks": {
+                "type": "string",
+                "description": "Concisely list the food/drinks that are being served at the event.",
+                "maxLength": 20,
+            },
+            "datetime": {
+                "type": "string",
+                "pattern": "\d{2}/\d{2}/\d{4} \d{1,2}:\d{2}",
+                "description": "The date and time of the event. If the month is november or december, use the year 2025. Otherwise, use the year 2026.",
+                "maxLength": 20,
+            },
+            "location": {
+                "type": "string",
+                "description": "The location of the event.",
+                "maxLength": 20,
+            },
         },
-        "required": ["title", "description"],
+        "required": ["event_name", "description", "has_food_or_drinks", "food_or_drinks", "datetime", "location"],
     }
-    instructions = "Extract event data from the input."
     input_data = context
 
     tool_list = [
         {
             "toolSpec": {
-                "name": "analyze_customer_review",
-                "description": "Analyze customer reviews.",
+                "name": tool_name,
+                "description": "Generate event data from the input.",
                 "inputSchema": {"json": schema},
             }
         }
     ]
-
-    # print("previous base64_image", base64_image[:10])
-    # media_type = base64_image.split(",")[0].split(":")[1].split("/")[1]
-    # base64_image = base64_image.split(",")[1]
-
-    # print("media_type", media_type)
-    # print("base64_image", base64_image[:10])
-
-    media_type = "image/jpeg"
-
-    # handle edge case
-    if media_type == "image/jpg":
-        media_type = "image/jpeg"
 
     messages = [
         {
             "role": "user",
             "content": [
                 {"text": json.dumps(input_data)},
+                {
+                    "image": {
+                        "format": "jpeg",
+                        "source": {
+                            "bytes": image_bytes
+                        }
+                    }
+                },
             ],
         },
-        # {
-        #     "role": "user",
-        #     "content": [
-        #         {
-        #             "type": "image",
-        #             "source": {
-        #                 "type": "base64",
-        #                 "media_type": media_type,
-        #                 "data": base64_image,
-        #             },
-        #         },
-        #     ],
-        # },
     ]
     response_converse = client.converse(
         modelId=MODEL_ID,
@@ -112,32 +111,44 @@ def generate_ai_data(context: str, base64_image: str):
         inferenceConfig={"maxTokens": MAX_TOKENS, "temperature": TEMPERATURE},
         toolConfig={
             "tools": tool_list,
-            "toolChoice": {"tool": {"name": "generate_event_data"}},
+            "toolChoice": {"tool": {"name": tool_name}},
         },
     )
 
-    print(response_converse)
+    response_content = (
+        response_converse.get("output", {})
+        .get("message", {})
+        .get("content", [{}])[0]
+        .get("toolUse", {})
+        .get("input", {})
+    )
+
+    return response_content
 
 
-def generate_and_save_ai_data(instagram_id: str, preview_data: dict):
+def generate_and_save_ai_data(instagram_id: str, instagram_url: str, preview_data: dict, table):
     image_url = preview_data.get("image", "")
     req = requests.get(image_url)
-    base64_image = base64.b64encode(req.content).decode("utf-8")
-
-    post_url = preview_data.get("url", "")
+    image_bytes = req.content
     title = preview_data.get("title", "")
     description = preview_data.get("description", "")
 
-    system_prompt = "You are an expert events summarizer."
+    prompt = "You are an expert events summarizer."
 
     context = {
-        "system_prompt": system_prompt,
+        "prompt": prompt,
         "title": title,
         "description": description,
     }
 
-    ai_data = generate_ai_data(context, base64_image)
-    pass
+    ai_data = {**generate_ai_data(context, image_bytes), "instagram_url": instagram_url}
+    table.update_item(
+        Key={"id": instagram_id},
+        UpdateExpression="set ai_data = :d",
+        ExpressionAttributeValues={":d": ai_data},
+    )
+    
+    return ai_data
 
 
 # dequeue oldest instagram id from queue
@@ -164,9 +175,15 @@ def dequeue_instagram_id():
 def handler(event, context):
     try:
         instagram_id = dequeue_instagram_id()
-        preview_data = fetch_and_save_instagram_preview(instagram_id)
+        instagram_url = f"https://www.instagram.com/p/{instagram_id}/"
+
+
+        dynamodb = boto3.resource("dynamodb")
+        table_name = os.environ["DYNAMO_EVENTS_TABLE_NAME"]
+        table = dynamodb.Table(table_name)
+        preview_data = fetch_and_save_instagram_preview(instagram_id, instagram_url, table)
         if has_food(preview_data):
-            generate_and_save_ai_data(instagram_id, preview_data)
+            generate_and_save_ai_data(instagram_id, instagram_url, preview_data, table)
         return {
             "statusCode": 200,
             "body": json.dumps(
